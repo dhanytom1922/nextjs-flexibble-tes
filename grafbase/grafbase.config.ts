ce6a134b8fe05756b0165c47c7035e7b994214af
dhanytom1922 <achmaddhany1922@gmail.com>
2023-11-19 15:51:30 +0700

    asd

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,24 +1,24 @@
 import { g, config, auth } from '@grafbase/sdk';
 
 // @ts-ignore
-const User = g.datasource('User', {
+const User = g.model('User', {
   name: g.string().length({ min: 2, max: 100 }),
   email: g.string().unique(),
   avatarUrl: g.url(),
   description: g.string().length({ min: 2, max: 1000 }).optional(),
   githubUrl: g.url().optional(),
   linkedinUrl: g.url().optional(), 
-  projects: g.relation(()=> Project).list().optional(),
+  // projects: g.relation(()=> Project).list().optional(),
 })
 
 // @ts-ignore
-const Project = g.datasource('Project', {
+const Project = g.model('Project', {
   title: g.string().length({ min: 3 }),
   description: g.string(), 
   image: g.url(),
   liveSiteUrl: g.url(),  
   githubUrl: g.url(), 
-  createdBy: g.relation(()=> User)
+  // createdBy: g.relation(()=> User)
 })
 
 export default config({

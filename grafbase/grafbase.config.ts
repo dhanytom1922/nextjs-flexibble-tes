de090eb2cbb5fa9779c9f4d10ab4da7c417ebb2d
dhanytom1922 <achmaddhany1922@gmail.com>
2023-11-19 15:46:06 +0700

    a

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -8,9 +8,7 @@ const User = g.model('User', {
   description: g.string().length({ min: 2, max: 1000 }).optional(),
   githubUrl: g.url().optional(),
   linkedinUrl: g.url().optional(), 
-  projects: g.relation(() => Project).list().optional(),
-}).auth((rules) => {
-  rules.public().read()
+  projects: g.relation(()=> Project).list().optional(),
 })
 
 // @ts-ignore
@@ -18,24 +16,15 @@ const Project = g.model('Project', {
   title: g.string().length({ min: 3 }),
   description: g.string(), 
   image: g.url(),
-  liveSiteUrl: g.url(), 
+  liveSiteUrl: g.url(),  
   githubUrl: g.url(), 
-  category: g.string().search(),
-  createdBy: g.relation(() => User),
-}).auth((rules) => {
-  rules.public().read()
-  rules.private().create().delete().update()
-})
-
-const jwt = auth.JWT({
-  issuer: 'grafbase',
-  secret:  g.env('NEXTAUTH_SECRET')
+  createdBy: g.relation(()=> User)
 })
 
 export default config({
   schema: g,
-  auth: {
-    providers: [jwt],
-    rules: (rules) => rules.private()
-  },
+  // auth: {
+  //   providers: [jwt],
+  //   rules: (rules) => rules.private()
+  // },
 })
